eed8e43d3091e8c4aff4bf55f7dfd8638072e1db
SebastienLaurentt <slaurent.26@gmail.com>
2024-08-26 15:14:06 +0200

    update readme

---
+++ components/Section.tsx
@@ -0,0 +1,29 @@
+interface SectionProps {
+  id?: string;
+  classname?: string;
+  marginBottom?: boolean;
+  marginTop?: boolean;
+  children: React.ReactNode;
+}
+
+const Section = ({
+  id,
+  classname,
+  marginBottom,
+  marginTop,
+  children,
+}: SectionProps) => {
+  const isMarginBottom = marginBottom ? "mb-32 md:mb-36 xl:mb-48" : "";
+  const isMarginTop = marginTop ? "mt-20 md:mt-24 xl:mt-28" : "";
+
+  return (
+    <section
+      id={id}
+      className={`${classname} ${isMarginBottom} ${isMarginTop} mx-auto max-w-7xl px-6 md:px-10 xl:mx-auto xl:px-16`}
+    >
+      {children}
+    </section>
+  );
+};
+
+export default Section;
