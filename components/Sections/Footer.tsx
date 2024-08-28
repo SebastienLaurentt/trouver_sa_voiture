95e0b6e3f8bcbd509528f72eceab281a4698e241
SebastienLaurentt <slaurent.26@gmail.com>
2024-08-28 15:29:17 +0200

    update footer and header

--- components/Sections/Footer.tsx
+++ components/Sections/Footer.tsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import { Atom } from "lucide-react";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className='text-center'>Trouver Sa voiture</div>
-  )
-}
+    <footer className="border-t">
+      <div className="mx-auto flex max-w-4xl flex-row justify-between py-12">
+        <div className="flex flex-col">
+          <div className="flex flex-row items-center gap-x-1">
+            <Atom size={28} className="text-primary" />
+            <span className="text-3xl font-semibold">TSV</span>
+          </div>
+          <span className="w-[250px] text-left">
+            Trouver sa voiture{" "}
+            <span className="font-bold text-primary">facilement</span> !
+          </span>
+        </div>
+        <div className="flex flex-row gap-x-8 text-left">
+          <div className="flex flex-col gap-y-3">
+            <span className="font-bold text-secondary-foreground">
+              {" "}
+              Nos voitures
+            </span>
+            <ul className="flex flex-col gap-y-1">
+              <Link href="/" className="xl:hover:text-foreground/80">
+                Classiques
+              </Link>
+              <Link href="/" className="xl:hover:text-foreground/80">
+                Premium
+              </Link>
+            </ul>
+          </div>
+          <div className="flex flex-col gap-y-3">
+            <span className="font-bold text-secondary-foreground">
+              Nos Services
+            </span>
+            <ul className="flex flex-col gap-y-1">
+              <Link href="/" className="xl:hover:text-foreground/80">
+                Estimation
+              </Link>
+              <Link href="/" className="xl:hover:text-foreground/80">
+                Acheter
+              </Link>
+              <Link href="/" className="xl:hover:text-foreground/80">
+                Vendre
+              </Link>
+            </ul>
+          </div>
+          <div className="flex flex-col gap-y-3">
+            <span className="font-bold text-secondary-foreground">
+              Plus d&apos;infos
+            </span>
+            <ul className="flex flex-col gap-y-1">
+              <Link href="/" className="xl:hover:text-foreground/80">
+                Equipe
+              </Link>
+              <Link href="/" className="xl:hover:text-foreground/80">
+                Contact
+              </Link>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="mx-auto flex max-w-4xl flex-row justify-between border-t py-6 text-sm text-secondary-foreground">
+        <div className="flex flex-row gap-x-4">
+          <Link href="/" className="xl:hover:font-semibold">
+            CGV
+          </Link>
+          <Link href="/" className="xl:hover:font-semibold">
+            Mentions légales
+          </Link>
+        </div>
+        <span>@2024 Trouver Sa Voiture</span>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- components/Sections/Header.tsx
+++ components/Sections/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { gsap } from "gsap";
-import Image from "next/image";
+import { Atom } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React, { useEffect } from "react";
@@ -10,43 +10,45 @@ const Header: React.FC = () => {
   const pathname = usePathname();
 
   useEffect(() => {
-    gsap.fromTo("#header", { opacity: 0 }, { opacity: 1, duration:1, delay:0.5 });
+    gsap.fromTo(
+      "#header",
+      { opacity: 0 },
+      { opacity: 1, duration: 1, delay: 0.5 }
+    );
   }, []);
 
   const links = [
     { name: "Accueil", href: "/", ariaLabel: "Aller à l'accueil" },
-    { name: "Premium", href: "/premium", ariaLabel: "Voir la section Premium" },
     {
-      name: "Nos véhicules",
+      name: "Véhicules",
       href: "/nos-vehicules",
       ariaLabel: "Voir nos véhicules",
     },
     {
-      name: "Services et prestations",
+      name: "Services",
       href: "/services",
       ariaLabel: "Voir les services et prestations",
     },
-    {
-      name: "Qui sommes nous",
-      href: "/about",
-      ariaLabel: "En savoir plus sur nous",
-    },
+
     { name: "Contact", href: "/contact", ariaLabel: "Nous contacter" },
   ];
 
   return (
     <header id="header" className="opacity-0 shadow-md">
-      <nav className="container mx-auto flex items-center justify-between px-6 py-2">
-        <Image src="/images/Rolls.svg" width={60} height={50} alt="logo" color="text-white"/>
-        <ul className="flex space-x-4">
+      <nav className="mx-auto flex max-w-5xl items-center justify-between px-6 py-4 ">
+        <div className="flex flex-row items-center gap-x-1">
+          <Atom size={28} className="text-primary" />
+          <span className="text-3xl font-semibold">TSV</span>
+        </div>
+        <ul className="flex">
           {links.map((link, idx) => (
             <li key={idx}>
               <Link
                 href={link.href}
                 aria-label={link.ariaLabel}
                 className={
-                  "xl:hover:text-primary" +
-                  (pathname === link.href ? " text-primary" : "")
+                  "xl:hover:bg-secondary rounded-sm px-2 py-2 transition-colors duration-500 ease-in-out" +
+                  (pathname === link.href ? "" : "")
                 }
               >
                 {link.name}
