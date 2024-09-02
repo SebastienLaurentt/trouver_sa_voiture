8f19efacb673f47bd4d5ea1fee9e2d37578b5ce4
SebastienLaurentt <slaurent.26@gmail.com>
2024-09-02 10:05:48 +0200

    import car card in classique page

--- app/(root)/voitures/classiques/page.tsx
+++ app/(root)/voitures/classiques/page.tsx
@@ -1,4 +1,6 @@
+import CarCard from "@/components/CarCard";
 import PageHeader from "@/components/PageHeader";
+import Section from "@/components/Section";
 
 const Classiques = () => {
   return (
@@ -8,6 +10,30 @@ const Classiques = () => {
         description="Send your product, marketing, and transactional email with Loops.
 One simple interface, for all your email."
       />{" "}
+      <Section classname="flex flex-row gap-x-4" marginTop marginBottom>
+        <CarCard
+          price={10000}
+          boiteType="Manuelle"
+          carType="Berline"
+          kmNumber={1000}
+          name="PORSCHE 911 CARRERA 4S"
+        />
+        <CarCard
+          price={10000}
+          boiteType="Manuelle"
+          carType="Berline"
+          kmNumber={1000}
+          name="PORSCHE 911 CARRERA 4S"
+        />
+        <CarCard
+          price={10000}
+          boiteType="Manuelle"
+          carType="Berline"
+          kmNumber={1000}
+          name="PORSCHE 911 CARRERA 4S"
+        />
+        
+      </Section>
     </main>
   );
 };
