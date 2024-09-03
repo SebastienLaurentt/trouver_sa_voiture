d11ac0b7029fdb29d137f4280be5d3bcc42d47b0
SebastienLaurentt <slaurent.26@gmail.com>
2024-09-03 16:43:16 +0200

    use grid

--- app/(root)/voitures/classiques/ClassiquesVehiculesList.tsx
+++ app/(root)/voitures/classiques/ClassiquesVehiculesList.tsx
@@ -4,25 +4,24 @@ import { getNonPremiumVehicles } from "@/lib/actions";
 
 const ClassiquesVehiculesList = async () => {
   const classicVehicles = await getNonPremiumVehicles();
+
   return (
-    <Section
-      classname="flex flex-row justify-between gap-x-4"
-      marginTop
-      marginBottom
-    >
-      {classicVehicles.map((vehicle, index) => (
-        <li key={index} className="list-none">
-          <CarCard
-            price={vehicle.price}
-            boiteType={vehicle.boiteType}
-            carType={vehicle.carType}
-            kmNumber={vehicle.kmNumber}
-            name={vehicle.name}
-            sold={vehicle.sold}
-            tag={vehicle.tag || ""}
-          />
-        </li>
-      ))}
+    <Section marginTop marginBottom>
+      <div className="grid grid-cols-1 justify-items-center gap-4 md:grid-cols-2 xl:grid-cols-3">
+        {classicVehicles.map((vehicle, index) => (
+          <li key={index} className="list-none">
+            <CarCard
+              price={vehicle.price}
+              boiteType={vehicle.boiteType}
+              carType={vehicle.carType}
+              kmNumber={vehicle.kmNumber}
+              name={vehicle.name}
+              sold={vehicle.sold}
+              tag={vehicle.tag || ""}
+            />
+          </li>
+        ))}
+      </div>
     </Section>
   );
 };
