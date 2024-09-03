e5ffe3c14bc6cd31935758ef3b256f872712c17c
SebastienLaurentt <slaurent.26@gmail.com>
2024-09-03 18:00:05 +0200

    update footer with cta

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,26 @@
 import { Atom } from "lucide-react";
 import Link from "next/link";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
 
 const Footer = () => {
   return (
-    <footer className=" border-t">
-      <div className="mx-auto flex flex-col justify-between gap-y-8 px-3 py-12 md:max-w-2xl  md:flex-row md:gap-y-0 md:px-0 lg:max-w-4xl">
+    <footer className="">
+      <div className="mx-auto mb-8 px-6 md:max-w-2xl md:px-0 lg:max-w-4xl">
+        <div className="flex flex-col items-center  gap-y-4 rounded-xl bg-muted  py-12 text-center md:gap-y-6 md:py-20">
+          <h2 className="font-semibold md:max-w-[400px]">
+            Vous avez besoin d&apos;être accompagné ?
+          </h2>
+          <p className="md:max-w-[360px]">
+            Laissez-nous votre email et nous recontactons au plus vite !{" "}
+          </p>
+          <div className="flex flex-row gap-x-2">
+            <Input placeholder="Votre email" />
+            <Button>Envoyer</Button>
+          </div>
+        </div>
+      </div>
+      <div className="mx-auto flex flex-col justify-between gap-y-8 px-6 py-12 md:max-w-2xl  md:flex-row md:gap-y-0 md:px-0 lg:max-w-4xl">
         <div className="flex flex-col">
           <div className="flex flex-row items-center gap-x-1">
             <Atom size={28} className="text-primary" />
