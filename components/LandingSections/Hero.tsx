1d43b8a78e6ece7938ba80b3a78315d4e52494eb
SebastienLaurentt <slaurent.26@gmail.com>
2024-09-04 09:39:46 +0200

    update css

--- components/LandingSections/Hero.tsx
+++ components/LandingSections/Hero.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { gsap } from "gsap";
+import { Star } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect } from "react";
@@ -24,7 +25,7 @@ const Hero = () => {
           Trouvez la voiture de{" "}
           <span className="text-primary"> vos rêves </span> en quelques clics !
         </h1>
-        <p className="subtitle my-4">
+        <p className="subtitle my-4 text-muted-foreground">
           {" "}
           Explorez les voitures neuves et d’occasion. Comparez et trouvez votre
           modèle idéal.{" "}
@@ -32,6 +33,29 @@ const Hero = () => {
         <Button>
           <Link href="/">Trouver ma voiture</Link>
         </Button>
+        <div className="mt-6 flex flex-col items-center justify-center gap-y-2 md:flex-row md:gap-x-3">
+          <div className="flex flex-col items-center">
+            <div className="flex flex-row items-center gap-x-1 font-bold">
+              <Image
+                src="/images/GoogleLogo.svg"
+                width={80}
+                height={80}
+                alt="Google Logo"
+              />
+              <span>5/5</span>
+            </div>
+            <div className="flex flex-row justify-center gap-x-0">
+              <Star fill="#f59e0b" color="#f59e0b" />
+              <Star fill="#f59e0b" color="#f59e0b" />
+              <Star fill="#f59e0b" color="#f59e0b" />
+              <Star fill="#f59e0b" color="#f59e0b" />
+              <Star fill="#f59e0b" color="#f59e0b" />
+            </div>
+          </div>
+          <span className="w-[200px] text-center text-sm md:text-left ">
+            +90 clients sont satisfaits par nos différents services
+          </span>
+        </div>
       </div>
       <div
         id="hero-img"
