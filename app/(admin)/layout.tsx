47b75844707d1851cd2214e90ad9396093925fab
SebastienLaurentt <slaurent.26@gmail.com>
2024-09-12 09:16:03 +0200

    update account header

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -9,7 +9,7 @@ export default async function RootLayout({
 }>) {
   return (
     <div className="flex min-h-screen flex-col">
-      <header className="flex items-center justify-between bg-gray-800 p-4 text-white">
+      <header className="mx-auto flex w-full items-center justify-between px-2 py-4 text-white md:max-w-2xl lg:max-w-3xl  xl:max-w-5xl">
         <Button asChild>
           <Link href="/">Home</Link>
         </Button>
