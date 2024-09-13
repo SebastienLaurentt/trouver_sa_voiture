34313f0523147a4620001e71512714baf1418534
SebastienLaurentt <slaurent.26@gmail.com>
2024-09-13 15:57:12 +0200

    add content

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,5 +1,7 @@
 import Services from "@/components/LandingSections/Services";
 import PageHeader from "@/components/PageHeader";
+import Section from "@/components/Section";
+import SectionHeader from "@/components/SectionHeader";
 
 const About = () => {
   return (
@@ -8,7 +10,19 @@ const About = () => {
         title="Rencontrez notre équipe dédiée"
         description="Passionnés d'automobile et experts du marché, nous mettons notre savoir-faire à votre service pour vous offrir une expérience unique et personnalisée."
       />
+      <Section marginTop>
+        <SectionHeader
+          tag="Notre agence"
+          description="A votre service depuis 2010"
+        />
+      </Section>
       <Services />
+      <Section marginBottom marginTop>
+        <SectionHeader
+          tag="Notre équipe"
+          description="Un groupe de passionnés à votre service"
+        />
+      </Section>
     </main>
   );
 };
