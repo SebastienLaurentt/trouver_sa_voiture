a9d6573a0ea220d92fdc9e31fbd1d3111d442ef6
SebastienLaurentt <slaurent.26@gmail.com>
2024-09-26 00:55:13 +0200

    add nav for account layout

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -11,14 +11,24 @@ export default async function RootLayout({
     <div className="flex min-h-screen flex-col">
       <header className="mx-auto flex w-full items-center justify-between px-2 py-4 text-white md:max-w-2xl lg:max-w-3xl  xl:max-w-5xl">
         <Button asChild>
-          <Link href="/">Home</Link>
+          <Link href="/">Retour Site</Link>
         </Button>
         <form action={logout}>
           {" "}
           <Button variant="destructive">Déconnexion</Button>
         </form>
       </header>
-      <div className="flex flex-1 flex-col justify-center">{children} </div>
+      <div className="flex flex-1 2xl:mt-20 ">
+        <nav className="flex flex-col gap-y-4 px-8">
+          <Button asChild variant="outline">
+            <Link href="/account/vehicules">Vehicules</Link>
+          </Button>
+          <Button asChild variant="outline">
+            <Link href="/account/estimations">Estimations</Link>
+          </Button>
+        </nav>
+        <div className="flex flex-1 flex-col">{children}</div>
+      </div>
     </div>
   );
 }
